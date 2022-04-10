69906b26b9f439947a555e7f7cd5c3c8544333ab
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-10 17:11:51 -0500

    estilos y logica para el evento del logo agregado

--- src/templates/header/header.js
+++ src/templates/header/header.js
@@ -1,7 +1,16 @@
-import './header.scss';
-import HEADER from './header.html';
+import "./header.scss";
+import HEADER from "./header.html";
+import PATHS from "../../router/routes";
+import { matchPath } from "../../utils/matchPath";
 
 export const loadHeader = () => {
-  const mainContainer = document.querySelector('.main-container');
-  return (mainContainer.innerHTML = HEADER);
+  const mainContainer = document.querySelector(".main-container");
+  mainContainer.innerHTML = HEADER;
+  logoEvent();
 };
+
+const logoEvent = () => {
+  const paths = PATHS;
+  const logo = document.querySelector(".icon");
+  logo.addEventListener("click", matchPath("/",paths));
+}
