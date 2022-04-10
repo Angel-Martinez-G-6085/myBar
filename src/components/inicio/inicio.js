9c1956257a42b5e89463eb6f3ee63e04419a35d2
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-10 17:10:34 -0500

    componente inicio terminado

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -1,7 +1,9 @@
 import './inicio.scss';
 import INICIO from './inicio.html';
+import { createCard } from '../drinks/drinks';
 
 export const loadInicio = () => {
   const header = document.querySelector('.header');
   header.insertAdjacentHTML('afterend', INICIO);
+  createCard(5);
 };
