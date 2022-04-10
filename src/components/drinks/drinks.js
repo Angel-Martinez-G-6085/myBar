35e899d976589d6ecf5201a6558251c37796326c
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-10 11:19:45 -0500

    funcion para detectar click en los botones

--- src/components/drinks/drinks.js
+++ src/components/drinks/drinks.js
@@ -2,7 +2,7 @@ import './drinks.scss';
 import DRINK_CARD from './drinks.html';
 import { setDrinkData } from '../../utils/getDrinks';
 
-export const createCard = (nDrinks) => {
+export const createCard = async (nDrinks) => {
     const slider = document.querySelector(".slider__container");
     const card  = DRINK_CARD;
     for (let i = 0; i<nDrinks; i++) {
@@ -10,5 +10,17 @@ export const createCard = (nDrinks) => {
             slider.insertAdjacentHTML("afterbegin", card);
         }
         setDrinkData();
+        let botones = await document.querySelectorAll(".btn");
+        botones = [...botones]
+        addButtonEvent({botones});
     }
+}
+
+export const addButtonEvent = ({botones}) => {
+    const haceClick = function() {
+        console .log(this)
+    }
+    botones.forEach(element => {
+        element.addEventListener("click", haceClick);
+    });
 }
