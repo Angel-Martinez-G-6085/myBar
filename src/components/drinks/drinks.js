8c6aa314a8633bd93a7b28335dc02f02abf15f22
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-10 12:33:48 -0500

    creacion de la ultima tarjeta

--- src/components/drinks/drinks.js
+++ src/components/drinks/drinks.js
@@ -1,21 +1,26 @@
 import './drinks.scss';
 import DRINK_CARD from './drinks.html';
+import DrinkListCard from './drinksList.html';
 import { setDrinkData } from '../../utils/getDrinks';
 
 export const createCard = async (nDrinks) => {
     const slider = document.querySelector(".slider__container");
     const card  = DRINK_CARD;
+    // Ciclo que crea todas las tarjetas y las rellena
     for (let i = 0; i<nDrinks; i++) {
         if(i <= nDrinks) {
             slider.insertAdjacentHTML("afterbegin", card);
         }
         setDrinkData();
+        // Encontramos todos los botones y les asignamos un efecto de click
         let botones = await document.querySelectorAll(".btn");
         botones = [...botones]
         addButtonEvent({botones});
     }
+    slider.insertAdjacentHTML("beforeend", DrinkListCard);
 }
 
+// Distingue a cada por si mismo
 export const addButtonEvent = ({botones}) => {
     const haceClick = function() {
         console .log(this)
