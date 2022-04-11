7bb45a717f229f1172a53a18e39ed5a238f98b52
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-10 23:08:23 -0500

    se elimino codigo innecesario

--- src/templates/header/header.js
+++ src/templates/header/header.js
@@ -1,7 +1,5 @@
 import "./header.scss";
 import HEADER from "./header.html";
-import PATHS from "../../router/routes";
-import { matchPath } from "../../utils/matchPath";
 
 export const loadHeader = () => {
   const mainContainer = document.querySelector(".main-container");
