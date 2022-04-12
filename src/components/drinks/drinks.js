fb6ec0801502399c08a133f32dcb6f0c52a5afe2
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-12 13:23:11 -0500

    componente modularizado

--- src/components/drinks/drinks.js
+++ src/components/drinks/drinks.js
@@ -1,8 +1,7 @@
 import './drinks.scss';
 import DRINK_CARD from './drinks.html';
-import DrinkListCard from './drinksList.html';
 import { DrinkData, setData } from '../../utils/getDrinks';
-import { loadSingleDrinkCard } from '../../utils/loadSingleDrinkCard';
+import { loadLastCard } from '../drinkListCard/drinkList';
 
 export const createCard = async (nDrinks) => {
     const arrayData = await DrinkData(nDrinks);
@@ -13,25 +12,7 @@ export const createCard = async (nDrinks) => {
         if(i <= nDrinks) {
             slider.insertAdjacentHTML("afterbegin", card);
         }
-        setData(arrayData);
-        // Encontramos todos los botones y les asignamos un efecto de click
-        let botones = await document.querySelectorAll(".btn");
-        botones = [...botones]
-        addButtonEvent({botones});
     }
-    slider.insertAdjacentHTML("beforeend", DrinkListCard);
-}
-
-// Distingue a cada por si mismo
-export const addButtonEvent = ({botones}) => {
-    const haceClick = function(evemto) {
-        // Obtiene el id del trago
-        const atributos = [...this.attributes];
-        const id = atributos[1].nodeValue;
-        loadSingleDrinkCard(id);
-    }
-
-    botones.forEach(element => {
-        element.addEventListener("click", haceClick);
-    });
+    await setData(arrayData);
+    loadLastCard();
 }
