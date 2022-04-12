fd65e63b1fe7d1e97cb26deb19839bd181d3b8c7
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-11 22:31:27 -0500

    se mejoro la carga de tarjetas

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -2,8 +2,8 @@ import './inicio.scss';
 import INICIO from './inicio.html';
 import { createCard } from '../drinks/drinks';
 
-export const loadInicio = () => {
+export const loadInicio = async () => {
   const header = document.querySelector('.header');
   header.insertAdjacentHTML('afterend', INICIO);
-  createCard(5);
+  await createCard(5);
 };
