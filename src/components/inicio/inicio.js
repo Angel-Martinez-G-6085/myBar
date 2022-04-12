8795f72e9af49eb4e9a28a481008d45b6a6a6352
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-04-12 13:26:48 -0500

    creamos e implementamos la funcion para todos los botones de las tarjetas

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -1,9 +1,11 @@
 import './inicio.scss';
 import INICIO from './inicio.html';
 import { createCard } from '../drinks/drinks';
+import { findButtons } from '../../utils/findButtons';
 
 export const loadInicio = async () => {
   const header = document.querySelector('.header');
   header.insertAdjacentHTML('afterend', INICIO);
   await createCard(5);
+  findButtons();
 };
---
+++ src/utils/findButtons.js
@@ -0,0 +1,16 @@
+// Distingue a cada por si mismo
+export const findButtons = () => {
+    let botones = document.querySelectorAll(".btn");
+    botones = [...botones];
+    botones.forEach(element => {
+        element.addEventListener("click", evento);
+    });
+}
+
+function evento(e){
+    let id = '';
+    let atributos = this.attributes;
+    atributos = [...atributos];
+    id = atributos[1].nodeValue;
+    console.log(id);
+}
